2291123094400d4ea587748431a897f97a74d61a
Yugapriya Vijayakumar <yugapriyav@systechusa.com>
2026-06-23 17:34:13 +0530

    first commit

---
+++ test_google.py
@@ -0,0 +1,6 @@
+# from playwright.sync_api import Page
+# def test_google(page:Page):
+#     page.goto("https://www.google.com")
+#     assert 'Google' in page.title()
+
+
---
+++ test_signin.py
@@ -0,0 +1,23 @@
+# from playwright.sync_api import Page
+
+# def test_signin(page:Page):
+#     page.goto("https://yugapriya1012-hub.github.io/Fullstack_Frontend/page/signin.html")
+
+#     page.click("#bakerTab")
+
+#     page.fill("#email","deepapriya1985@gmail.com")
+#     page.fill("#password","Deepa@123")
+
+#     page.on("dialog",lambda dialog:dialog.accept())
+
+#     page.click("#submitBtn")
+
+#     page.wait_for_url("**/page/seller/seller_home.html")
+
+#     print(page.url)
+#     # print(page.title())
+
+#     assert 'page/seller/seller_home.html' in page.url
+ 
+
+
